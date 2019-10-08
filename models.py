0e38117792b401a6645116460774902664290af1
edodo1337 <eldar100@Mail.ru>
2019-10-08 13:09:47 +0300

    'init'

--- models.py
+++ models.py
@@ -4,6 +4,12 @@ from datetime import datetime
 import re
 
 
+
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+        )
+
 def slugify(s):
     pattern = r'[^\w+]'
     return  re.sub(pattern, '-', s)
@@ -14,6 +20,8 @@ class Post(db.Model):
     slug = db.Column(db.String(140), unique=True)
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
+
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
 
     def __init__(self, *args, **kwargs):
         super(Post, self).__init__(*args, **kwargs)
